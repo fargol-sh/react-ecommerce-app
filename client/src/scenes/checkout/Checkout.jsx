d5b7fe31d4df774ea43bad25a9fc4426d8f5083a
Fargol Shirvanifar <fargolsh79@gmail.com>
2023-08-19 21:23:54 +0330

    add checkout, payment, shipping, and address form components

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -1,5 +1,232 @@
+import { useSelector } from 'react-redux';
+import { Box, Button, Stepper, Step, StepLabel } from '@mui/material';
+import { Formik } from 'formik';
+import { useState } from 'react';
+import * as yup from 'yup';
+import { shades } from '../../theme';
+import Shipping from './Shipping';
+import Payment from './Payment';
+import { loadStripe } from '@stripe/stripe-js';
+
+
+const stripePromise = loadStripe(
+    "pk_test_51Ngt5aFGjg3vuVzqORLHUkuRbfu5nDBesLwQYjV7Z4X5RuHk2JNxemz6sUtSALn3DOzKytvSmRI9YM9nBiyZ2S7I007RN6jZPH"
+)
+
+const initialValues = {
+    billingAddress: {
+        firstName: "",
+        lastName: "",
+        country: "",
+        street1: "",
+        street2: "",
+        city: "",
+        state: "",
+        zipCode: "",
+    },
+    shippingAddress: {
+        isSameAddress: true, // This will determine if the address is the same as billing address, all other information will be copied here.
+        firstName: "",
+        lastName: "",
+        country: "",
+        street1: "",
+        street2: "",
+        city: "",
+        state: "",
+        zipCode: "",
+    },
+    email: "",
+    phoneNumber: ""
+};
+
+const checkoutSchema = [
+    // we need to pass an object to shape() because
+    // initial values for user is in object format.
+    // Also, because we're using Stepper to have 2 steps 
+    // of validating the inputs(one for billing address 
+    // and shipping address and one for email and phone number)
+    // there are two yup.object().shape(). So the based on 
+    // activeStep value, we will be authenticated in each step.
+    yup.object().shape({
+        billingAddress: yup.object().shape({
+            firstName: yup.string().required("required"),
+            lastName: yup.string().required("required"),
+            country: yup.string().required("required"),
+            street1: yup.string().required("required"),
+            street2: yup.string(),
+            city: yup.string().required("required"),
+            state: yup.string().required("required"),
+            zipCode: yup.string().required("required"),
+        }),
+        shippingAddress: yup.object().shape({
+            isSameAddress: yup.boolean(),
+            // This code will check if the 'isSameAddress' is
+            // false, then makes the input required, but if it's 
+            // true, it won't be required anymore!
+            firstName: yup.string().when("isSameAddress", {
+                is: false,
+                then: yup.string().required("required")
+            }),
+            lastName: yup.string().when("isSameAddress", {
+                is: false,
+                then: yup.string().required("required")
+            }),
+            country: yup.string().when("isSameAddress", {
+                is: false,
+                then: yup.string().required("required")
+            }),
+            street1: yup.string().when("isSameAddress", {
+                is: false,
+                then: yup.string().required("required")
+            }),
+            street2: yup.string(),
+            city: yup.string().when("isSameAddress", {
+                is: false,
+                then: yup.string().required("required")
+            }),
+            state: yup.string().when("isSameAddress", {
+                is: false,
+                then: yup.string().required("required")
+            }),
+            zipCode: yup.string().when("isSameAddress", {
+                is: false,
+                then: yup.string().required("required")
+            }),
+        }),
+    }),
+    yup.object().shape({
+        email: yup.string().required('required'),
+        phoneNumber: yup.string().required('required')
+    }),
+
+]
+
 const Checkout = () => {
-    return <div>Checkout</div>
+    const [activeStep, setActiveStep] = useState(0);
+    const cart = useSelector((state) => state.cart.cart);
+    const isFirstStep = activeStep === 0;
+    const isSecondStep = activeStep === 1;
+
+    const handleFormSubmit = async (values, actions) => {
+        setActiveStep(activeStep + 1);
+
+        // copies the billing address onto shipping address
+        if (isFirstStep && values.shippingAddress.isSameAddress) {
+            actions.setFieldValue("shippingAddress", {
+                ...values.billingAddress,
+                isSameAddress: true
+            })
+        }
+
+        if (isSecondStep) {
+            makePayment(values);
+        }
+
+        // reset the validation when submitting the form.
+        // Also, we get 'actions' from Formik.
+        actions.setTouched({});
+    }
+
+    async function makePayment(values) {
+        const stripe = await stripePromise;
+        const requestBody = {
+            userName: [values.firstName, values.lastName].join(" "),
+            email: values.email,
+            products: cart.map(({id, count}) => ({
+                id,
+                count
+            }))
+        };
+
+        const response = await fetch('http://localhost:1337/api/orders', {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json'},
+            body: JSON.stringify(requestBody)
+        });
+
+        const session = await response.json();
+        await stripe.redirectToCheckout({
+            sessionId: session.id
+        })
+    }
+
+    return (
+        <Box width="80%" m="100px auto">
+            <Stepper activeStep={activeStep} sx={{ m: "20px 0"}}>
+                <Step>
+                    <StepLabel>Billing</StepLabel>
+                </Step>
+                <Step>
+                    <StepLabel>Payment</StepLabel>
+                </Step>
+            </Stepper>
+            <Box>
+                <Formik
+                    onSubmit={handleFormSubmit}
+                    initialValues={initialValues}
+                    validationSchema={checkoutSchema[activeStep]}
+                >
+                    {({values, errors, touched, handleBlur, handleChange, handleSubmit, setFieldValue}) => (
+                        <form onSubmit={handleSubmit}>
+                            {isFirstStep && (
+                                <Shipping
+                                    values={values}
+                                    errors={errors}
+                                    touched={touched}
+                                    handleBlur={handleBlur}
+                                    handleChange={handleChange}
+                                    setFieldValue={setFieldValue}
+                                />
+                            )}
+
+                            {isSecondStep && (
+                                <Payment
+                                    values={values}
+                                    errors={errors}
+                                    touched={touched}
+                                    handleBlur={handleBlur}
+                                    handleChange={handleChange}
+                                    setFieldValue={setFieldValue}
+                                />
+                            )}
+
+                            <Box display="flex" justifyContent="space-between" gap="50px">
+                                {isSecondStep && (
+                                    <Button
+                                        fullWidth
+                                        color="primary"
+                                        variant="contained"
+                                        sx={{
+                                            backgroundColor: shades.primary[200],
+                                            boxShadow: "none",
+                                            color: "white",
+                                            borderRadius: 0,
+                                            padding: "15px 40px"
+                                        }}
+                                        onClick={() => setActiveStep(activeStep - 1)}
+                                    >Back</Button>
+                                )}
+                                <Button
+                                    fullWidth
+                                    type="submit"
+                                    color="primary"
+                                    variant="contained"
+                                    sx={{
+                                        backgroundColor: shades.primary[400],
+                                        boxShadow: "none",
+                                        color: "white",
+                                        borderRadius: 0,
+                                        padding: "15px 40px"
+                                    }}
+                                    onClick={() => setActiveStep(activeStep - 1)}
+                                >{isFirstStep ? "Next" : "Place Order"}</Button>
+                            </Box>
+                        </form>
+                    )}
+                </Formik>
+            </Box>
+        </Box>
+    );
 };
 
 export default Checkout;
